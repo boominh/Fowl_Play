e7ac74ddca28b04e4b20e3793552d74c18087d51
boominh <125905500+boominh@users.noreply.github.com>
2023-09-18 20:25:55 +0200

    Update PlayerMovement.cs

--- Assets/Mich Playhouse/Player/PlayerMovement.cs
+++ Assets/Mich Playhouse/Player/PlayerMovement.cs
@@ -6,37 +6,27 @@ public class PlayerMovement : MonoBehaviour
 {
 
     Rigidbody2D rb2D;
-    public float duckMaxSpeed = 3.5f;
-    public float acceleration = 11;
-    public float deacceleration = 7;
+    public float duckMaxSpeed = 6f;
+    public float acceleration = 11f;
+    public float deacceleration = 7f;
 
     Vector2 userInput;
     Vector2 velocity;
-    Vector2 duckPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-
         rb2D = GetComponent<Rigidbody2D>();
-       
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity = userInput * duckMaxSpeed;
-
         userInput.x = Input.GetAxisRaw("Horizontal");
         userInput.y = Input.GetAxisRaw("Vertical");
+        userInput.Normalize()
 
-        //velocity += userInput * acceleration;
-
-        //if( velocity.sqrMagnitude > duckMaxSpeed * duckMaxSpeed) { velocity.Normalize(); velocity *= duckMaxSpeed; }
-
-        //if (userInput.sqrMagnitude == 0) { velocity *= 2f - deacceleration; }
-
-        //duckPosition += rb2D.velocity;
+        velocity = userInput * duckMaxSpeed;
 
         rb2D.velocity = velocity;
     }
