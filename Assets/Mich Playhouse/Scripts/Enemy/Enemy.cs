5d78e874aecef113b30685549819890f43615add
boominh <michynuggy@gmail.com>
2023-09-26 15:41:13 +0200

    Update Enemy.cs

--- Assets/Mich Playhouse/Scripts/Enemy/Enemy.cs
+++ Assets/Mich Playhouse/Scripts/Enemy/Enemy.cs
@@ -7,68 +7,78 @@ public class Enemy : MonoBehaviour
     public GameObject projectile;
     Animator animator;
 
-    float walkDuration;
-
     float randomX;
     float randomY;
+
     float width;
     float height;
     
+    float walkTime;
     float walkTimer;
-    float fireTimer;
-    public float fireRate;
     
+    float fireRate;
+    float fireRateTimer;
 
-    bool startBlasting = false;
+    bool okToShoot = false;
+
     Vector3 shootingPosition;
     Vector3 startingPosition;
 
     void Start()
     {
         startingPosition = transform.position;
+        walkTime = Random.Range(0.5f, 1f);
+        animator = GetComponent<Animator>();
       
         width = Camera.main.orthographicSize * Camera.main.aspect;
         height = Camera.main.orthographicSize;
 
         randomX = Random.Range(width/2, width);
         randomY = Random.Range(-height + 1, height - 1);
-        shootingPosition = new Vector3(randomX, randomY); // Optimize with insideunitcircle later
+        shootingPosition = new Vector3(randomX, randomY);
 
-        //enables duck to fire as soon as shootingPosition is reached
-        fireTimer = fireRate;
-
-        //randomize
-        walkDuration = Random.Range(0.5f, 1f);
-        
-        animator = GetComponent<Animator>();
+        // Enables duck to fire as soon as shootingPosition is reached
+        fireRateTimer = fireRate;
     }
+
     void Update()
     {
-        //Fire with fire rate
-        if (fireTimer > fireRate && startBlasting) 
-        {
-            fireTimer = 0;
-            Instantiate(projectile, transform.position, transform.rotation);
-            fireRate = Random.Range(2, 4);
-            animator.Play("duck_attack");
-        }
-
-        // Make walk to position
         if (transform.position != shootingPosition)
         {
-            walkTimer += Time.deltaTime;
-            float t = Mathf.Clamp01(walkTimer / walkDuration);
-
-            Vector3 newPosition = Vector3.Lerp(startingPosition, shootingPosition, t);
-            transform.position = newPosition;
+            GoToShootingPosition();
         }
 
-        // When to start shooting
         if (transform.position == shootingPosition)
         {
-            startBlasting = true;
+            okToShoot = true;
         }
 
-        fireTimer += Time.deltaTime;
+        if (okToShoot)
+        {
+            EnemyFire();
+        }
+
+        fireRateTimer += Time.deltaTime;
+    }
+
+    private void GoToShootingPosition()
+    {
+        walkTimer += Time.deltaTime;
+        float t = Mathf.Clamp01(walkTimer / walkTime);
+
+        Vector3 newPosition = Vector3.Lerp(startingPosition, shootingPosition, t);
+        transform.position = newPosition;
+    }
+
+    private void EnemyFire()
+    {
+        if (fireRateTimer > fireRate)
+        {
+            fireRateTimer = 0;
+            fireRate = Random.Range(2, 4);
+
+            animator.Play("duck_attack");
+            Instantiate(projectile, transform.position, transform.rotation);
+        }
     }
 }
